558d97ecdd7b50dbd3e0cb4b349e9e9fd0797430
sourabhrockz <sourabh20051998@gmail.com>
2024-06-04 21:34:51 +0200

    fetch the album name data from firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import { AlbumList } from "./Components/AlbumList";
 import NavBar from "./Components/NavBar";
-import AlbumForm from "./Components/AlbumForm";
 import React from "react";
 
 function App() {
   return (
     <>
       <div className="navDiv"><NavBar /></div>
-      <div className="app_content"><AlbumForm/></div>
+      <AlbumList />
     </>
   );
 }
