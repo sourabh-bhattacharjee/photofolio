654f6ec035ad427342780079801f9eec3b2dd34f
sourabhrockz <sourabh20051998@gmail.com>
2024-06-05 17:01:27 +0200

    add image list from data fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import { AlbumList } from "./Components/AlbumList";
 import NavBar from "./Components/NavBar";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AlbumForm from "./Components/AlbumForm";
+import { db } from "./firebaseInit";
+import { collection, query, getDocs } from "firebase/firestore";
+import ImageList from "./Components/ImageList";
+
 
 function App() {
+    const [isShowAddAlbum , setIsShowAddAlbum] = useState(false);
+    const [toggleAddCancelAlbum , setToggleAddCancelAlbum] = useState(true);
+    const [getName, setGetName] = useState([]);
+    useEffect(() =>{
+        let data = [];
+            const fetchDoc = async () =>{
+            const q = query(collection(db, "ImageAlbum"));
+            const querySnapshot = await getDocs(q);
+            console.log(querySnapshot);
+            querySnapshot.forEach((doc) => {
+                data.push(doc.data().AlbumName)
+              });
+
+            setGetName(data);
+        }
+        fetchDoc()
+    },[])
+    function handleAlbumClick(){
+        console.log(getName);
+        setIsShowAddAlbum(!isShowAddAlbum);
+        setToggleAddCancelAlbum(!toggleAddCancelAlbum);
+    }
+    const getAlbumNameForm = (data) => {
+        let temp = [...getName,data] 
+        console.log("TEMP",temp);
+        setGetName(temp);
+        // console.log(getName);
+    }
   return (
     <>
       <div className="navDiv"><NavBar /></div>
-      <AlbumList />
+      <div className="app_content">
+            {isShowAddAlbum ? <div><AlbumForm returnAlbumName={getAlbumNameForm}/></div> : null}
+            <div>
+                <div className="albumList_top">
+                    <h3>Your albums</h3>
+                    {toggleAddCancelAlbum? <button onClick={handleAlbumClick}>Add Album</button>
+                    : <button className="cancel" onClick={handleAlbumClick}>cancel</button>}
+                </div>
+                < ImageList  AlbumName={getName}/>
+            </div>
+        </div>
     </>
   );
 }
